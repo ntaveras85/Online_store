b769fcc6a514e0ee842ab10a5af8aa25dad86f73
ntaveras <noel.taverascastillo@gmail.com>
2022-05-10 12:56:19 -0400

    Cambio realizado en el modulo de compra

--- src/app/Online_store/checkout/thank-you-page/thank-you-page.component.ts
+++ src/app/Online_store/checkout/thank-you-page/thank-you-page.component.ts
@@ -2,7 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-thank-you-page',
-  templateUrl: './thank-you-page.component.html',
+  template: `
+    <app-header></app-header>
+    <div class="container">
+            <h1 class="title">Gracias por tu compra</h1>
+        <p class="content">
+            Tu compra ha sido enviada
+        </p>
+        <span>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab ratione
+            sit aperiam incidunt, consequuntur placeat porro iusto amet.  
+            Recusandae optiolaudantium consequatur saepe est odit nemo. Animi sapiente odit amet.
+        </span>
+    </div>
+  `,
   styleUrls: ['./thank-you-page.component.css']
 })
 export class ThankYouPageComponent implements OnInit {
